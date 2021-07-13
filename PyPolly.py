be403aedf35f6d3506e807293ee44109d5e9442f
fmorote01 <fernandomorote@gmail.com>
2021-07-13 10:39:34 -0400

    First Version

---
+++ PyPolly.py
@@ -0,0 +1,22 @@
+# The Data we need to retrieve.
+# 1. The toal number of votes cast
+# 2. A complete list of candidates who reeived votes
+# 3. The percentage of votes per candidate
+# 4. Total n umber of votes per candidate
+# 5. The winner of teh election
+
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
